b1ccf893a122c13ab1d01e877910ec00b1a79d5b
Ethan Frigon <ebfrigon@gmail.com>
2026-03-20 16:19:00 -0700

    chore: configure Vitest for unit tests

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    environment: 'node',
+    include: ['tests/unit/**/*.test.ts'],
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      '~/': new URL('./src/', import.meta.url).pathname,
+    },
+  },
+});
